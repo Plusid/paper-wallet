43a43ac3a65e931b6dae0e9a0fba1bcd7ba3c218
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2021-01-04 19:26:21 +0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ class Config {
         }
 
         if (!this.get("network")) {
-            this.set("network", "Devnet");
+            this.set("network", "Mainnet");
         }
 
         this.load();
